b6a8b3dc2535cc951e8c57a521942dfaae2e410b
casariz <danielfelipedfco@gmail.com>
2025-06-08 11:50:07 -0500

    feat: add CardExperience and Experience components with basic structure and tests, update SkillCard styling, and enhance Skills section layout

---
+++ src/app/components/card-experience/card-experience.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-experience',
+  imports: [],
+  templateUrl: './card-experience.component.html',
+  styleUrl: './card-experience.component.css'
+})
+export class CardExperienceComponent {
+
+}
