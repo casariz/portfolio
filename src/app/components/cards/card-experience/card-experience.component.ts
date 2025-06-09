5c34cc44136d2f9d236441bf79005e7d69123d9a
casariz <danielfelipedfco@gmail.com>
2025-06-08 22:15:14 -0500

    feat: enhance CardExperience component with location and period details, and update imports

--- src/app/components/cards/card-experience/card-experience.component.ts
+++ src/app/components/cards/card-experience/card-experience.component.ts
@@ -1,12 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { Experience } from '../../../interfaces/experience';
+import { CommonModule } from '@angular/common';
+import { LucideAngularModule, MapPin, Calendar } from 'lucide-angular';
+
 
 @Component({
   selector: 'app-card-experience',
-  imports: [],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './card-experience.component.html',
   styleUrl: './card-experience.component.css'
 })
 export class CardExperienceComponent {
+  readonly mapPin = MapPin;
+  readonly calendar = Calendar;
+
+  
+
   @Input() experience!: Experience;
+  @Input() index!: number;
 }
